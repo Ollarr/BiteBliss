e068fba1242213cc3aaf2ef4d1cfb53815061469
Oluwasegun <oluyedis@gmail.com>
2023-10-12 08:28:36 +0100

    Added the Category screen

---
+++ android/app/src/main/kotlin/com/example/bitebliss/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bitebliss
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/categories.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoriesSCreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Choose Category"),
+        
+      ),
+      body: ,
+    );
+  }
+}
