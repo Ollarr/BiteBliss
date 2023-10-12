880ca0c7f18edf64034fdc0248418af2443cf4a5
Oluwasegun <oluyedis@gmail.com>
2023-10-12 18:12:07 +0100

    Added the meal screen to display the list of meals by category

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -9,41 +9,49 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+        margin: const EdgeInsets.all(8),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        clipBehavior: Clip.hardEdge,
+        elevation: 4,
         child: InkWell(
-      onTap: () {},
-      child: Stack(children: [
-        FadeInImage(
-          placeholder: MemoryImage(kTransparentImage),
-          image: NetworkImage(meal.imageUrl),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
-              color: Colors.black45,
-              child: Column(
-                children: [
-                  Text(
-                    meal.title,
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
-                    softWrap: true,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    children: [],
-                  )
-                ],
-              )),
-        )
-      ]),
-    ));
+          onTap: () {},
+          child: Stack(children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+                  color: Colors.black45,
+                  child: Column(
+                    children: [
+                      Text(
+                        meal.title,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [],
+                      )
+                    ],
+                  )),
+            )
+          ]),
+        ));
   }
 }
