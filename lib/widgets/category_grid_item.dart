2c09c2d4d333c9740200eee7d7a2def3bd602b45
Oluwasegun <oluyedis@gmail.com>
2023-10-12 17:33:34 +0100

    Added the meal screen to display the list of meals by category

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:bitebliss/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem(
+      {super.key, required this.category, required this.onSelectCategory});
 
   final Category category;
-
+  final void Function() onSelectCategory;
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        onSelectCategory();
+      },
       splashColor: Theme.of(context).colorScheme.primary,
       borderRadius: BorderRadius.circular(16),
       child: Container(
