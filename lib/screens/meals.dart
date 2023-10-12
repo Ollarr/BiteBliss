a114f7e5d33b318554f3c771ec763fb8c1f86d7a
Oluwasegun <oluyedis@gmail.com>
2023-10-12 17:34:09 +0100

    Added the meal screen to display the list of meals by category

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,10 +1,41 @@
+import 'package:bitebliss/models/meal.dart';
+import 'package:bitebliss/widgets/meal_item.dart';
 import 'package:flutter/material.dart';
 
 class MealScreen extends StatelessWidget {
-  const MealScreen({super.key});
+  const MealScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    return const Text("SIngle meal");
+    Widget content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+              meal: meals[index],
+            ));
+    if (meals.isEmpty) {
+      content = const Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Uh oh... nothing found!",
+                style: TextStyle(color: Colors.white70)),
+            SizedBox(
+              height: 16,
+            ),
+            Text("Try selecting a different category!",
+                style: TextStyle(color: Colors.white70))
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content);
   }
 }
