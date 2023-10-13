bb9342c2efe08c112a8fa7677231b01785a49194
Oluwasegun <oluyedis@gmail.com>
2023-10-13 16:21:20 +0100

    Added tab-based navigation functionality Passed function through multiple layers of widget for state mangement

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,10 +2,14 @@ import 'package:bitebliss/data/data.dart';
 import 'package:bitebliss/screens/meals.dart';
 import 'package:bitebliss/widgets/category_grid_item.dart';
 import 'package:bitebliss/models/category.dart';
+import 'package:bitebliss/models/meal.dart';
+
 import 'package:flutter/material.dart';
 
 class CategoriesSCreen extends StatelessWidget {
-  const CategoriesSCreen({super.key});
+  const CategoriesSCreen({super.key, required this.onToggleFavorite});
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals
@@ -15,38 +19,35 @@ class CategoriesSCreen extends StatelessWidget {
     // Navigator.push(context, route);
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) =>
-            MealScreen(title: category.title, meals: filteredMeals),
+        builder: (ctx) => MealScreen(
+            title: category.title,
+            meals: filteredMeals,
+            onToggleFavorite: onToggleFavorite),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Choose Category"),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        // N/B: The CategoryGridItems can be rendered using either map or for in loop
-        // children: availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      // N/B: The CategoryGridItems can be rendered using either map or for in loop
+      // children: availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
 
-        children: [
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              onSelectCategory: () {
-                _selectCategory(context, category);
-              },
-            )
-        ],
-      ),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          )
+      ],
     );
   }
 }
