fde5b17941d0e803c34a1a39f89e1fe4c645486c
Oluwasegun <oluyedis@gmail.com>
2023-10-13 16:18:12 +0100

    Added tab-based navigation functionality Passed function through multiple layers of widget for state mangement

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,78 @@
+import 'package:bitebliss/screens/categories.dart';
+import 'package:bitebliss/screens/meals.dart';
+import 'package:bitebliss/models/meal.dart';
+
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _toggleFavoriteStatus(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
+
+    void showToggleFavoriteMessage(String message) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(message)));
+    }
+
+    if (isExisting) {
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      showToggleFavoriteMessage("Meal removed from favorite");
+    } else {
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
+      showToggleFavoriteMessage("Meal added to favorite");
+    }
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesSCreen(
+      onToggleFavorite: _toggleFavoriteStatus,
+    );
+    var activePageTitle = "Categories";
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealScreen(
+        meals: _favoriteMeals,
+        onToggleFavorite: _toggleFavoriteStatus,
+      );
+      activePageTitle = "Favorites";
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          currentIndex: _selectedPageIndex,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.set_meal), label: "Categories"),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites"),
+          ]),
+    );
+  }
+}
