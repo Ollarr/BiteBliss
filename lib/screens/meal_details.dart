39c304f919a01d3cfa277c2ae34958d9ca84764a
Oluwasegun <oluyedis@gmail.com>
2023-10-13 09:18:02 +0100

    Added the single meal screen to display the meal details dynamically

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -9,30 +9,51 @@ class MealDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text(meal.title)),
-      body: Column(
-        children: [
-          Image.network(
-            meal.imageUrl,
-            fit: BoxFit.cover,
-            height: 300,
-            width: double.infinity,
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Text("ingredients",
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground)),
-          const SizedBox(
-            height: 16,
-          ),
-          for (final ingredient in meal.ingredients)
-            Text(ingredient,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground))
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+              height: 300,
+              width: double.infinity,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Text("Ingredients",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold)),
+            const SizedBox(
+              height: 16,
+            ),
+            for (final ingredient in meal.ingredients)
+              Text(ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground)),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              "Steps",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            for (final step in meal.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
