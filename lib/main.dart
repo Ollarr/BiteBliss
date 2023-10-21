40008c5114d16a87355a85af08abe531da0358fc
Oluwasegun <oluyedis@gmail.com>
2023-10-21 08:47:46 +0100

    Added  a simple meal provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // import 'package:bitebliss/screens/categories.dart';
 import 'package:bitebliss/screens/tabs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,7 +15,11 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
@@ -22,6 +27,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const TabsScreen());
+    return MaterialApp(
+      theme: theme,
+      home: const TabsScreen(),
+    );
   }
 }
