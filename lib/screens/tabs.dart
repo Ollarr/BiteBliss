c9a70b5d91cd309ac9ec1e5d39eb1c354ff878a9
Oluwasegun <oluyedis@gmail.com>
2023-10-21 08:48:16 +0100

    consumed the meal provider

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,11 +1,12 @@
-import 'package:bitebliss/data/data.dart';
+// import 'package:bitebliss/data/meals.dart';
+import 'package:bitebliss/providers/meals_provider.dart';
 import 'package:bitebliss/screens/categories.dart';
 import 'package:bitebliss/screens/filters.dart';
 import 'package:bitebliss/screens/meals.dart';
 import 'package:bitebliss/models/meal.dart';
 import 'package:bitebliss/widgets/main_drawer.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // N/B: This a way of naming global variables i.e adding k before a variable identifier.
 const kInitialFilters = {
@@ -15,16 +16,16 @@ const kInitialFilters = {
   Filter.vegan: false
 };
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<TabsScreen> createState() {
+  ConsumerState<TabsScreen> createState() {
     return _TabsScreenState();
   }
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
 
@@ -82,7 +83,10 @@ class _TabsScreenState extends State<TabsScreen> {
 // final availableMeals = dummyMeals.where((meal) => {
 //   if (_selectedFilters[Filters.glutenFree]! && !meal.isGlutenFree)
 // });
-    final availableMeals = dummyMeals.where((meal) {
+
+    final meals = ref.watch(mealsProvider);
+
+    final availableMeals = meals.where((meal) {
       if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
         return false;
       }
