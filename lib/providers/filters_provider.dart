45757131c328074ae91ac8ac4b9c8ec5abd47313
Oluwasegun <oluyedis@gmail.com>
2023-10-23 08:32:25 +0100

    added the functionality to update the state of the filtered meal

---
+++ lib/providers/filters_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
+
+class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  FiltersNotifier()
+      : super({
+          Filter.glutenFree: false,
+          Filter.lactoseFree: false,
+          Filter.vegetarian: false,
+          Filter.vegan: false
+        });
+// This function is to update the state of each individual filter based on the current state.
+  void setFilter(Filter filter, bool isActive) {
+    state = {
+      ...state,
+      filter: isActive,
+    };
+  }
+// While this function is to update/override existing state of all the filters.
+
+  void setFilters(Map<Filter, bool> chosenFilters) {
+    state = chosenFilters;
+  }
+}
+
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+        (ref) => FiltersNotifier());
